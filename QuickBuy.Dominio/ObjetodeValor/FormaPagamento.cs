f27ec3864e08aa1ac18e0bff4dff84b73cf2e22f
marciolobianco <marcio.asp.net@gmail.com>
2020-02-26 20:24:57 -0300

    criação de entidades

---
+++ QuickBuy.Dominio/ObjetodeValor/FormaPagamento.cs
@@ -0,0 +1,32 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetodeValor
+{
+  public   class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool ehBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; } 
+        }
+        public bool ehCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool ehDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+
+        public bool NãoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinodo; }
+        }
+    }
+}
